6ad63e215ecfd7a4e7b88846cdd0eb55ccda34b1
Daniel <85527001+danchc@users.noreply.github.com>
2023-05-23 11:59:08 +0200

    Aggiornamenti
    Risolto problema Infrastructure -> Jpa -> Mapper
    Aggiunti controlli su NULL

--- company_project_rest/src/main/java/it/itc/company_project_rest/infrastructure/jpa/mapper/employee/EmployeeMapper.java
+++ company_project_rest/src/main/java/it/itc/company_project_rest/infrastructure/jpa/mapper/employee/EmployeeMapper.java
@@ -4,10 +4,14 @@ import it.itc.company_project_rest.domain.model.department.DepartmentId;
 import it.itc.company_project_rest.domain.model.department.DepartmentModel;
 import it.itc.company_project_rest.domain.model.employee.EmployeeId;
 import it.itc.company_project_rest.domain.model.employee.EmployeeModel;
+import it.itc.company_project_rest.domain.model.project.ProjectModel;
+import it.itc.company_project_rest.infrastructure.entity.department.DepartmentEntity;
 import it.itc.company_project_rest.infrastructure.entity.employee.EmployeeEntity;
+import it.itc.company_project_rest.infrastructure.entity.project.ProjectEntity;
 import it.itc.company_project_rest.infrastructure.jpa.mapper.department.DepartmentMapper;
 import it.itc.company_project_rest.infrastructure.jpa.mapper.project.ProjectMapper;
 import java.util.LinkedList;
+import java.util.List;
 
 /*
 
@@ -22,40 +26,62 @@ public class EmployeeMapper {
 
     /* fromModelToEntity */
     public EmployeeEntity fromModelToEntity(EmployeeModel employeeModel){
+        DepartmentEntity departmentEntity = null;
+        List<ProjectEntity> projectEntityList = new LinkedList<>();
+
+        if(employeeModel.getDepartmentModel() != null){
+             departmentEntity = departmentMapper.fromModelToEntity(employeeModel.getDepartmentModel());
+        }
+
+        if(employeeModel.getProjectModelList() != null){
+            projectEntityList = employeeModel.getProjectModelList().stream().map(projectMapper::fromModelToEntity).toList();
+        }
+
         return EmployeeEntity.builder()
                 .employeeId(employeeModel.getEmployeeId().getEmployeeId())
                 .email(employeeModel.getEmail())
                 .name(employeeModel.getName())
                 .surname(employeeModel.getSurname())
-                /* da rivedere
+                /* da rivedere */
                 .departmentEntity(
-                        departmentMapper.fromModelToEntity(employeeModel.getDepartmentModel())
+                        departmentEntity
                 )
                 .projectEntityList(
-                        employeeModel.getProjectModelList().stream().map(projectMapper::fromModelToEntity).toList()
-                )*/.build();
+                        projectEntityList
+                ).build();
 
     }
 
     /* fromEntityToModel */
     public EmployeeModel fromEntityToModel(EmployeeEntity employeeEntity){
+
+        DepartmentModel departmentModel = null;
+        List<ProjectModel> projectModelList = new LinkedList<>();
+
+        if(employeeEntity.getDepartmentEntity() != null) {
+            departmentModel = new DepartmentModel(
+                    new DepartmentId(
+                            employeeEntity.getDepartmentEntity().getDepartmentId()
+                    ),
+                    employeeEntity.getName()
+            );
+        }
+
+        if(employeeEntity.getProjectEntityList() != null) {
+            projectModelList = employeeEntity.getProjectEntityList().stream().map(projectMapper::fromEntityToModel).toList();
+        }
+
         return EmployeeModel.builder()
                 .employeeId(new EmployeeId(employeeEntity.getEmployeeId()))
                 .email(employeeEntity.getEmail())
                 .name(employeeEntity.getName())
                 .surname(employeeEntity.getSurname())
-                /*
                 .departmentModel(
-                        new DepartmentModel(
-                                new DepartmentId(employeeEntity.getDepartmentEntity().getDepartmentId()),
-                                employeeEntity.getDepartmentEntity().getName()
-                            )
-                        )
+                        departmentModel
+                )
                 .projectModelList(
-                        new LinkedList<>(
-                                employeeEntity.getProjectEntityList().stream().map(projectMapper::fromEntityToModel).toList()
-                        )
-                )*/
+                        projectModelList
+                )
                 .build();
     }
 
